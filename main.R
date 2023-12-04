2522d4177ecf1352c6015c4790947eafbe60e29a
JacekZgud <jacek.zgud2@gmail.com>
2023-12-04 16:38:10 +0100

    main engine reorganization

--- main.R
+++ main.R
@@ -5,7 +5,10 @@ library(data.table)
 source("./structs/functions.R")
 n = 3
 d = 4
+#set vertices names
 NodeNames = LETTERS[1:d]
+
+#define relation structure
 ParentStructure = matrix(nrow = d, ncol = d, data = 0)
 diag(ParentStructure) = 1
 rownames(ParentStructure) = colnames(ParentStructure) = NodeNames
@@ -15,84 +18,30 @@ ParentStructure[2, 4] = 1
 #ParentStructure[3,2] = 1
 
 NoParents = rowSums(ParentStructure)
-TransitionProbabilities = vector("list", c(d))
-names(TransitionProbabilities) = NodeNames
 
-Trans_prob = function(Nodenames, ParentStructure, n, d) {
-  prob_vector_names = paste("prob", c(0:(n - 1)), sep = '_')
-  for (node in NodeNames) {
-    parents = NodeNames[which(ParentStructure[node,] == 1)]
-    n0_parents = NoParents[parents]
-    temp = expand.grid(rep(list(0:c(n - 1)), length(n0_parents)))
-    colnames(temp) = parents
-    #temp['prob1'] = runif(nrow(temp))
-    #print(runif(n*nrow(temp)))
-    prob_matr = matrix(
-      runif(n * nrow(temp)),
-      nrow = nrow(temp),
-      ncol = n,
-      byrow = TRUE
-    )
-    colnames(prob_matr) = prob_vector_names
-    prob_matr = t(apply(prob_matr, 1, function(x)
-      x / sum(x)))
-    TransitionProbabilities[[node]] = data.table(cbind(temp, prob_matr))
-    #print(as.matrix(cbind(temp,prob_matr)))
-    #print(data.table(cbind(temp,prob_matr)))
-    setkeyv(TransitionProbabilities[[node]], parents)
-  }
-  return(TransitionProbabilities)
-  
-}
 TransitionProbabilities = Trans_prob(Nodenames, ParentStructure, n, d)
 
 TransitionProbabilities[["A"]]
 
 #########################################
-#Macierz przejśćia
+#transition matrix
+#takes some time to calculate, approx 30 sec for n=3 and d=4. 
 
 
-
-start = Sys.time()
 Trans = trans_matrix(n, d)
-end = Sys.time()
-end - start
+stationary_probability(Trans,n,d)
 
-rowSums(Trans)
-A = t(Trans - diag(ncol = n ^ d, nrow = n ^ d))
-A = rbind(A, rep(1, n ^ d))
-b = c(rep(0, n ^ d), 1)
-res_statio = qr.solve(A, b)
-res_statio = cbind(expand.grid(rep(list(0:c(
-  n - 1
-)), d)), res_statio)
-colnames(res_statio) = c(NodeNames, 'statio_prob')
-sum(res_statio$statio_prob)
-res_statio
 
-# for (vertex in 1:d)
-# {TransitionProbabilities[[vertex]] = numeric(2^NoParents[vertex])}
-#
-# TransitionProbabilities[[1]]=runif(2)
-# TransitionProbabilities[[2]]=runif(4)
-# TransitionProbabilities[[3]]=runif(4)
 
-#TransitionProbabilities[[1]]=c(0.01,0.99)
-#TransitionProbabilities[[2]]=c(0.1,0.1,0.9,0.9)
-#TransitionProbabilities[[3]]=c(0.1,0.1,0.9,0.9)
+#######
+#markov process symulation
 
-m = 10 ^ 4
-XZY = matrix(nrow = m, ncol = d)
-colnames(XZY) = NodeNames
-State = rep(0, d)
-for (i in 1:m) {
-  State = Step(State)
-  XZY[i, ] = State
-}
+XZY = markov_sim(TransitionProbabilities,10^3,d,n)
 
-X = XZY[, "X"]
-Z = XZY[, "Z"]
-Y = XZY[, "Y"]
+X = XZY[, "A"]
+Z = XZY[, "B"]
+Y = XZY[, "C"]
+
 hist(X)
 # Stationary distribution of X
 table(X) / m
@@ -115,9 +64,3 @@ XY. = table(X, Y)
 XY.
 mosaicplot(XY.)
 chisq.test(XY.)$expected
-###########################################
-#Generowanie macierzy przejśćia
-#Znalezienie rozkładu stacjonarnego [układ zależmy,pominąć jedno równanie i zastąpić je sumowaniem do jedynki]
-#
-#https://mimuw.edu.pl/~wniem/Sym_Stoch/SyStoMC.pdf str 145
-#
--- structs/functions.R
+++ structs/functions.R
@@ -1,18 +1,56 @@
+# define structure for markov chain
+
+Trans_prob = function(Nodenames, ParentStructure, n, d) {
+  TransitionProbabilities = vector("list", c(d))
+  names(TransitionProbabilities) = NodeNames
+  prob_vector_names = paste("prob", c(0:(n - 1)), sep = '_')
+  for (node in NodeNames) {
+    parents = NodeNames[which(ParentStructure[node, ] == 1)]
+    n0_parents = NoParents[parents]
+    temp = expand.grid(rep(list(0:c(n - 1)), length(n0_parents)))
+    colnames(temp) = parents
+    prob_matr = matrix(
+      runif(n * nrow(temp)),
+      nrow = nrow(temp),
+      ncol = n,
+      byrow = TRUE
+    )
+    colnames(prob_matr) = prob_vector_names
+    prob_matr = t(apply(prob_matr, 1, function(x)
+      x / sum(x)))
+    TransitionProbabilities[[node]] = data.table(cbind(temp, prob_matr))
+    setkeyv(TransitionProbabilities[[node]], parents)
+  }
+  return(TransitionProbabilities)
+}
+
+
 # One step of Markov Chain
 #
-Step = function(State) {
+Step = function(State,TransitionProbabilities) {
   NewState = c()
   prob_columns = paste("prob", c(1:(n - 1)), sep = "_")
   for (vertex in NodeNames) {
-    Parents = (ParentStructure[vertex, ] == 1)
+    Parents = (ParentStructure[vertex,] == 1)
     ParentState = State[Parents]
-    #print(as.list(ParentState))
-    #loc =  apply(TransitionProbabilities[[vertex]][NodeNames[Parents]], 1, function(x) all(c(x) == ParentState ))
     Prob = TransitionProbabilities[[vertex]][as.list(ParentState), ..prob_columns]
-    #Prob = TransitionProbabilities[[vertex]][loc,prob_columns]
     NewState[vertex] = sum(runif(1) > cumsum(as.vector(Prob)))
   }
   return(NewState)
+}
+
+#waterfall for simulation
+#markov process symulation
+
+markov_sim = function(structure,m,d,n){
+  XZY = matrix(nrow = m, ncol = d)
+  colnames(XZY) = NodeNames
+  State = rep(0, d)
+  for (i in 1:m) {
+    State = Step(State,structure)
+    XZY[i, ] = State
+  }
+  return(XZY)
 }
 
 #calculate transition matrix
@@ -30,6 +68,18 @@ trans_matrix = function(n, d) {
   )
   return(Trans2)
 }
+#solve stationary equation to obtain stationary probability distribution
+stationary_probability = function(transition_matrix,n,d){
+  A = t(transition_matrix - diag(ncol = n ^ d, nrow = n ^ d))
+  A = rbind(A, rep(1, n ^ d))
+  b = c(rep(0, n ^ d), 1)
+  res_statio = qr.solve(A, b)
+  res_statio = cbind(expand.grid(rep(list(0:c(
+    n - 1
+  )), d)), res_statio)
+  colnames(res_statio) = c(NodeNames, 'statio_prob')
+  return(res_statio)
+}
 
 
 #calculate transition probability for given configuration
@@ -37,7 +87,7 @@ trans_matrix = function(n, d) {
 prob_transition = function(x) {
   vec = c()
   for (node in NodeNames) {
-    parents = NodeNames[which(ParentStructure[node, ] == 1)]
+    parents = NodeNames[which(ParentStructure[node,] == 1)]
     Parents = paste(parents, "(t-1)", sep = "")
     
     ParentState = x[Parents]
@@ -48,12 +98,9 @@ prob_transition = function(x) {
     # prob = TransitionProbabilities[[node]][loc,paste("prob",node_state,sep="_")]
     
     prob = setDT(TransitionProbabilities[[node]])[as.list(ParentState), ..prob_col]
-    #print(prob)
     vec[node] = as.double(prob)
   }
-  #print(as.double(prod(vec)))
-  
+
   return(as.double(prod(vec)))
   
 }
-
